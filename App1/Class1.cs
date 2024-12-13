639f53d5d96c30530624a9adf1c9f8bbb27ff142
omare <omare@OMAR_ESAM>
2024-12-13 09:40:57 +0200

    Add project files.

---
+++ App1/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    internal class Class1
+    {
+        static void Main()
+        {
+            #region Question1
+            //Console.Write("Enter a number: ");
+            //int number = int.Parse(Console.ReadLine());
+            //Console.WriteLine("You entered: " + number);
+            #endregion
+            #region Question2
+
+            //int x = 15;
+            //int y = 10;
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+
+            //x = y; 
+
+            //Console.WriteLine("after aqual");
+
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+
+            //x++;
+            //Console.WriteLine("After change");
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+
+
+            #endregion
+            #region Question3
+            //Point P1;
+            //Point P2;
+
+            //P1 = new Point();
+            //P2 = new Point();
+            //P1.x = 100;
+            //P1.y = 150;
+            //P2.x = 200;
+            //P2.y = 250;
+            //Console.WriteLine(P1.x);
+            //Console.WriteLine(P1.y);
+            //Console.WriteLine(P2.x);
+            //Console.WriteLine(P2.y);
+            //Console.WriteLine("After Equal");
+            //P1 = P2;
+            //Console.WriteLine(P1.x);
+            //Console.WriteLine(P1.y);
+            //Console.WriteLine(P2.x);
+            //Console.WriteLine(P2.y);
+            //Console.WriteLine("After Change");
+            //P1.x = 50;
+            //P1.y = 70;
+            //Console.WriteLine(P1.x);
+            //Console.WriteLine(P1.y);
+            //Console.WriteLine(P2.x);
+            //Console.WriteLine(P2.y);
+            #endregion
+        }
+    }
+}
+
